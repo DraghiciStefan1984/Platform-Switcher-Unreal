023a15b0a201fdd9224baa316da98adb379c3bcc
StefanDraghici1984 <draghicistefan1984@gmail.com>
2018-12-16 19:54:52 +0200

    start coding

---
+++ PlatformSwitcher/Source/PlatformSwitcher/MainGameMode.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MainGameMode.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+
+void AMainGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMainGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AMainGameMode::OnSwitch()
+{
+
+}
---
+++ PlatformSwitcher/Source/PlatformSwitcher/MainGameMode.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMSWITCHER_API AMainGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	bool switched;
+
+	void OnSwitch();
+};
